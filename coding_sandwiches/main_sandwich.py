a8a5d447a3ef8d07e5bc775b0e62bc3b6ac5f0bd
kate <kate.yu@marsden.school.nz>
2023-06-09 10:17:04 +1200

    Finished sprint 1 and editted some of the planning site

--- coding_sandwiches/main_sandwich.py
+++ coding_sandwiches/main_sandwich.py
@@ -1,25 +1,33 @@
-def print_sandwiches():
-    my_list = [
-        "Halloumi Sandwich" "$16",
-        "Pork Banh Mi" "$19",
-        "Roasted Beetroot Sandwich" "$14",
-        "Sausage and Egg Sandwich" "$15.5",
-        "Smoked Salmon Deluxe" "$16",
-        "Ham Sandwich" "$14",
-        "Buttermilk Chicken Sandwich" "$19",
-        "'Lucky Beef' Steak Sandwich" "$19.5",
-        "Milanese and Gremolata Panini" "$17",
-        "Snapper Sandwich" "$20",
-        "Jalapeño and Cheddar Sandwich" "$15"
-    ]
-
-    [print(x) for x in my_list]
 
 def input_action():
-    my_input = input("Please choose an option")
+    my_input = input("Please choose an option").upper()
     return my_input
 
-def menu():
+
+def print_menu(M):
+    for i in range(0, len(M)):
+        output = "{}: {:<40} ${:.2f}".format(i,M[i][0], M[i][1])
+        print(output)
+    return None
+
+def main():
+    # sandwiches list lives here
+    my_list = [
+        ["Halloumi Sandwich", 16],
+        ["Pork Banh Mi", 19],
+        ["Roasted Beetroot Sandwich", 14],
+        ["Sausage and Egg Sandwich", 15.5],
+        ["Smoked Salmon Deluxe", 16],
+        ["Ham Sandwich", 14],
+        ["Buttermilk Chicken Sandwich", 19],
+        ["'Lucky Beef' Steak Sandwich", 19.5],
+        ["Milanese and Gremolata Panini", 17],
+        ["Snapper Sandwich", 20],
+        ["Jalapeño and Cheddar Sandwich", 15]
+    ]
+
+
+
     my_menu = '''
     A : print menu
     B : quit
@@ -29,10 +37,11 @@ def menu():
         print(my_menu)
         user_choice = input_action()
         if user_choice == "A":
-            print_sandwiches()
+            print_menu(my_list)
         elif user_choice == "B":
             run = False
             print("Thank you for visiting Marsden Gourmet Sandwich Bar")
         else:
             print("Unrecognised entry, this is not an option")
-menu()
+
+main()
