9c8601892ccc48a402db39c709a5775f53ecc7f6
kate <kate.yu@marsden.school.nz>
2023-06-12 13:46:53 +1200

    added start of order function

--- coding_sandwiches/main_sandwich.py
+++ coding_sandwiches/main_sandwich.py
@@ -1,4 +1,25 @@
+def get_integer(m):
+    get_result = True
+    while get_result:
+        try:
+            my_integer = int(input(m))
+        except ValueError:
+            print("you have not entered an integer")
+            continue
+        get_result = False
+    return my_integer
 
+def get_string(m):
+    my_string = input(m)
+    return my_string
+
+def order(O):
+        c = get_integer("How many sandwiches would you like to order?")
+        i = 0
+        while i < c:
+            my_order = get_string("Please enter the index number of the sandwich you would like to order:")
+            O.append(my_order)
+            i += 1
 def input_action():
     my_input = input("Please choose an option").upper()
     return my_input
@@ -26,19 +47,22 @@ def main():
         ["Jalapeño and Cheddar Sandwich", 15]
     ]
 
-
+    my_order = []
 
     my_menu = '''
-    A : print menu
-    B : quit
+    P : Print menu
+    O : Order
+    Q : Quit
     '''
     run = True
     while run is True:
         print(my_menu)
         user_choice = input_action()
-        if user_choice == "A":
+        if user_choice == "P":
             print_menu(my_list)
-        elif user_choice == "B":
+        elif user_choice == "O":
+            order(my_order)
+        elif user_choice == "Q":
             run = False
             print("Thank you for visiting Marsden Gourmet Sandwich Bar")
         else:
