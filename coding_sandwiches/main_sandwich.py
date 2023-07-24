2819084329f571810847a8e013222d83c1e30756
kate <kate.yu@marsden.school.nz>
2023-07-25 11:02:52 +1200

    Sprint 4 complete + some of sprint 5

--- coding_sandwiches/main_sandwich.py
+++ coding_sandwiches/main_sandwich.py
@@ -1,15 +1,52 @@
 """Sandwiches ordering program"""
 
-def get_integer(m):
+
+def phone_integer(m):
+    # doc - typing
+    """validating that the phone number is only integers
+
+    :param m: string (message)
+    :return: integer (multiple)
+    """
+    # validating for an integer for the phone number input
+    my_integer = 0
     get_result = True
     while get_result:
-        try:
-            my_integer = int(input(m))
-        except ValueError:
-            print("you have not entered an integer")
+        my_phone = input(m)
+        if not my_phone.isdigit():
+            print("Please enter only numbers")
             continue
         get_result = False
     return my_integer
+
+def get_integer(m, lower, upper):
+    # doc - typing
+    """validating the integer inputs when choosing from indexes
+
+    :param m: string (message)
+    :param lower: integer (smallest index (0))
+    :param upper: integer (largest index (len))
+    :return: integer
+    """
+    # validating integer inputs that have a specific range e.g for when I ask the customer to input an index number from an options list
+    num = 0
+    getting_integer = True
+    while getting_integer is True:
+        try:
+            num = int(input(m))
+        except ValueError:
+            print("Not an integer")
+            continue
+
+        if num < lower:
+            # validating if the number the user has entered is too low (below the minimum)
+            print("Your value is too low")
+        elif num > upper:
+            # validating if the number the user has entered is too high (above maximum)
+            print("Your value is too high")
+        else:
+            getting_integer = False
+    return num
 
 def get_string(m):
     my_string = input(m).upper()
@@ -18,6 +55,7 @@ def get_string(m):
 
 
 def confirmation(m, chars=['Y','N'] ):
+    # doc - typing
     """Get specified character input.
 
     :param m: string (message)
@@ -25,37 +63,66 @@ def confirmation(m, chars=['Y','N'] ):
     :return: string (1 character)
     """
     while True:
+        # to ask the user if they are sure they want to do this action
         choice = input(m).upper()
         if choice not in chars:
-            print( "Please enter {}".format(' or '.join(chars) ) )
+            print("Please enter {}".format(' or '.join(chars)))
         else:
             return choice
 
 
 def print_menu(M):
+    # doc - typing
+    """
+    :param M: list (menu)
+    :return: none
+    """
+    # printing the sandwich menu
     print("-" * 80)
     for i in range(0, len(M)):
+        # formatting the sandwich menu
         output = "{}: {:<40} ${:.2f}".format(i,M[i][0], M[i][1])
         print(output)
     print("-" * 80)
     return None
 
-def order(O,M):
+def order(O,M, lower, upper):
+    # doc - typing
+    """order function
+    :param O: list (my_order)
+    :param M: list (menu)
+    :param lower: integer (lowest index)
+    :param upper: integer (highest index)
+    :return:
+    """
+
+    # asking user for integer inputs that correlate to the sandwich index numbers
     print_menu(M)
-    index_num = get_integer("Please enter the number of the sandwich you would like: -> ")
-    sand_num = get_integer("Please enter how many of this sandwich you would like: -> ")
+    m = "Please enter the number of the sandwich you would like: -> "
+    index_num = get_integer(m, lower, upper)
+    sand_num = int(input("Please enter how many of this sandwich you would like: -> "))
     output = "You have ordered the {} of the {}".format(sand_num,M[index_num][0])
     print(output)
+    # adding the information above into a temporary list, to insert this information into the master list
     temp = [M[index_num][0], M[index_num][1], sand_num]
     O.insert(0, temp)
 
 
 def review(O,C):
+    # doc - typing
+    """reviewing the customer's whole order
+    :param O: list (my_order)
+    :param C: list (string and integer mixed) - (customer_details)
+    :return:
+    """
+    # function that allows the user to review their order
+    # setting the total to 0 so the total amount due is added onto the total everytime
     total = 0
     print("-" * 80)
     for i in range(0, len(O)):
         sub_total = O[i][2]*O[i][1]
         total += sub_total
+        # formatting how the review is printed out
         order_item = "x{:<5} {:<40} @ ${:5.2f} each = ${:5.2f} ".format(O[i][2], O[i][0], O[i][1], sub_total)
         print(order_item)
 
@@ -82,16 +149,30 @@ def review(O,C):
 
 
 
-def edit_order(O):
+def edit_order(O, lower, upper):
+    # doc - typing
+    """editing the former order
+    :param O: list (my_order)
+    :param lower: integer (lowest index)
+    :param upper: integer (highest index)
+    :return:
+    """
+    # function that allows the user to edit their order
     print("-" * 80)
     for i in range(0, len(O)):
+        # formatting what they have ordered so far
         output = "{}: {:<40} x{}".format(i, O[i][0], O[i][2])
         print(output)
     print("-" * 80)
-    my_index = get_integer("Please enter the index number to update the amount of this sandwich you would like: -> ")
-    new_amount = get_integer("Enter the new number of this sandwich you would like: -> ")
+    # asking for what sandwich they'd like to edit
+    m = "Please enter the index number to update the amount of this sandwich you would like: -> "
+    my_index = get_integer(m, lower, upper)
+    print("* To remove a sandwich completely, you can enter 0 for the next question")
+    # asking user the new amount they would like
+    new_amount = int(input("Enter the new number of this sandwich you would like: -> "))
     old_amount = O[my_index][2]
     O[my_index][2] = new_amount
+    # confirming/ letting the user know what has been replaced - the new changes
     output_message = "{} has now been changed to {}.".format(old_amount, new_amount)
     print(output_message)
     if new_amount == 0:
@@ -100,7 +181,14 @@ def edit_order(O):
         delete_message = "{} has been removed from your order". format(sandwich_name)
         print(delete_message)
 
+
 def details(O, C):
+    # doc - typing
+    """gathering the data inputs of the customer details to print on receipt
+    :param O: list (my_order)
+    :param C: list ((string and integers) - (customer_details)
+    :return: none and deliver
+    """
     # test if details are already there
     if len(C) != 0:
         message = "You have already entered details, do you want to enter them again (Y/N)? --> "
@@ -113,14 +201,16 @@ def details(O, C):
         elif choice == "N":
             return None
 
-
-    name = input("Please enter your name: --> ")
-    phone_number = get_integer("Please enter your phone number: --> ")
+    # asking the customer to input their details
+    name = (input("Please enter your name: --> "))
+    m = "Please enter your phone number: --> "
+    phone_number = phone_integer(m)
+    # adding these inputs into the customer_details list so it can be kept and printed out in the review function
     C.append(name)
     C.append(phone_number)
     message = "Enter 'P' to pick up your order, to get your order delivered enter 'D': -> "
     deliver = confirmation(message, ["P","D"])
-
+    # checking if they would like pick up or delivery
     if deliver == "P":
         message = "Pick up is free, we'll see you in store later"
         print(message)
@@ -128,23 +218,56 @@ def details(O, C):
         output = "Here are your details - Name: {},   Phone number: {}".format(name, phone_number)
         print(output)
         return None
+    # if they chose delivery then it also asks the user to enter their address and automatically adds $3 to their order total
     elif deliver == "D":
         # showing the user the $3 has been added to their order
         d_message = "Delivery is an extra $3 charge, this will automatically be added to your order total"
         # ask for address
         print(d_message)
         O.append(["Delivery", 3, 1])
-    # get customer details
-    # ask for customer name, phone number, address
         address = input("Please enter an address for delivery: --> ")
         C.append(address)
+        # confirming/ letting the user know what their details are that have been recorded
         output = "Here are your details - Name: {},   Phone number: {},   Address: {}". format(name, phone_number, address)
         print(output)
     else:
         return deliver
 
+def extras(E):
+    # doc - typing
+    """
+    :param A: list (menu)
+    :return: none
+    """
+    # printing the sandwich menu
+    print("-" * 80)
+    for i in range(0, len(E)):
+        # formatting the sandwich menu
+        output = "{}: {:<40} ${:.2f}".format(i, E[i][0], E[i][1])
+        print(output)
+    print("-" * 80)
+    return None
+def add(E, O, lower, upper):
+    extras(E)
+    a = "Please enter the number of the extra you would like: -> "
+    index_num = get_integer(a, lower, upper)
+    num = int(input("Please enter how many servings of this extra you would like: -> "))
+    output = "You have added {} of {} to your sandwich".format(num, E[index_num][0])
+    print(output)
+    # adding the information above into a temporary list, to insert this information into the master list
+    temp = [E[index_num][0], E[index_num][1], num]
+    O.insert(0, temp)
+
+
 
 def confirm_order(O,C):
+    # doc - typing
+    """confirming the order so the next user can start a new one (clears all lists from previously asked inputs)
+    :param O: list (my_order)
+    :param C: list (string and integers) - (customer_details)
+    :return: none
+    """
+    # basically printing a full receipt
     review(O,C)
     confirm = get_string("Would you like the confirm your order (Y/N)? --> ")
     if confirm == "Y":
@@ -156,23 +279,24 @@ def confirm_order(O,C):
         return None
 
 def input_action():
+    # doc -typing
+    """
+    :return: my_input (1 character)
+    """
+    # getting the user input as to what they would like to do
     my_input = get_string("Please choose an option: -> ")
     return my_input
 
 
 
 def main():
+    # name , price , quantity
     my_order = []
-    my_order = [
-        # name , price , quantity
-        ["Roasted Beetroot Sandwich", 14, 3],
-        ["Jalapeño and Cheddar Sandwich", 15, 7],
-    ]
+
     # Name, phone number, address
     customer_details = []
 
     # sandwiches list lives here
-
     my_list = [
         ["Halloumi Sandwich", 16],
         ["Pork Banh Mi", 19],
@@ -187,15 +311,27 @@ def main():
         ["Jalapeño and Cheddar Sandwich", 15]
     ]
 
+    extras = [
+        ["Cheese", 1.5],
+        ["Bacon", 3],
+        ["Avocado", 2],
+        ["Salsa", 2],
+        ["Lettuce", 1.5],
+        ["Tomato", 1.5],
+        ["Cucumber", 1],
+        ["Grilled Chicken", 5]
+    ]
 
-    my_menu=['P : Print menu',
-             'O : Order',
-             'R : Review',
-             'E : Edit order',
-             'D : Details',
-             'C : Confirm order',
-             'Q : Quit'
-             ]
+    # options menu
+    my_menu = ['P : Print menu',
+               'O : Order',
+               'A : Add extras',
+               'E : Edit order',
+               'D : Details',
+               'R : Review',
+               'C : Confirm order',
+               'Q : Quit'
+               ]
 
     run = True
     new_order = True
@@ -210,15 +346,17 @@ def main():
         if user_choice == "P":
             print_menu(my_list)
         elif user_choice == "O":
-            order(my_order, my_list)
+            order(my_order, my_list, 0, len(my_list)-1)
         elif user_choice == "R":
             review(my_order, customer_details)
         elif user_choice == "E":
-            edit_order(my_order)
+            edit_order(my_order, 0, len(my_order)-1)
         elif user_choice == "D":
             details(my_order, customer_details)
         elif user_choice == "C":
             confirm_order(my_order, customer_details)
+        elif user_choice == "A":
+            add(extras, my_order, 0, len(extras)-1)
         elif user_choice == "Q":
             run = False
             print("Thank you for visiting Marsden's Gourmet Sandwich Bar")
